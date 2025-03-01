0dff2b0a22d34bdeb9d0800885c14fa9d0ee9138
igorcol <igor.colombini@gmail.com>
2025-02-28 22:17:31 -0300

    style: basic emails page

--- src/app/emails/page.tsx
+++ src/app/emails/page.tsx
@@ -1,6 +1,24 @@
+'use client'
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
+
+    function handleSendEmails() {
+        console.log('>> handleSendEmails')
+        /* 
+        Para cada USER (dar fetch em user):
+            Pegar email dele
+            Enviar o Email
+        */
+
+    }
+
     return (
-        <h1>INGRESSOS</h1>
+        <div className="space-y-5">
+            <Button onClick={()=>handleSendEmails()}>
+                Enviar Emails
+            </Button>
+            <p>Não implementado</p>
+        </div>
     )
 }
