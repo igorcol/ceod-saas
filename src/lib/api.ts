babbd3fa725e1bb4969b0ebddd9d013da665773d
igorcol <igor.colombini@gmail.com>
2025-02-28 22:26:49 -0300

    fix: lib/api types

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,10 @@
 // TODO: fetchUsers()
 
-let API_URL = process.env.NEXT_PUBLIC_API_URL
-let API_PORT = process.env.NEXT_PUBLIC_API_PORT
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+const API_PORT = process.env.NEXT_PUBLIC_API_PORT
 
 interface ApiResponse {
-    [key: string]: any;
+    [key: string]:  any;
 }
 
 // Fetch
@@ -13,37 +13,13 @@ async function fetchData(get_data:string)  {
     return data
 }
 
-// Retorna uma lista com todos os emais
-async function GetEmailsFromDb() {
-    try {
-        // Fazendo uma requisição GET para a API
-        const response = await fetch('http://localhost:3030/inscritos');
-        
-        if (!response.ok) {
-            throw new Error('Erro na requisição: ' + response.statusText);
-        }
-
-        const data = await response.json(); // Converte a resposta para JSON
-        
-        if (!data ) {
-            throw new Error('Dados de inscritos não encontrados');
-        }
-
-        // Extraindo apenas os e-mails dos inscritos
-        const emails: string[] = data.map((inscrito: { EMAIL: string }) => inscrito.EMAIL);
-        return emails; // Retorna os e-mails
-
-    } catch (error) {
-        console.error('Erro:', error);
-    }
-}
 
 // GET FROM DB 
 async function GetFromDb(get_data: string): Promise<ApiResponse | undefined> {
     try {
         
         // Fazendo uma requisição GET para a API | https://localhost:3030/users
-        let api_users_url = `${API_URL}:${API_PORT}/${get_data}`
+        const api_users_url = `${API_URL}:${API_PORT}/${get_data}`
         console.log(`Fetching "${get_data}" from ${api_users_url}`)
 
         const response = await fetch(api_users_url);
@@ -66,5 +42,4 @@ async function GetFromDb(get_data: string): Promise<ApiResponse | undefined> {
 
 export {
     fetchData,
-    GetEmailsFromDb
 } 
