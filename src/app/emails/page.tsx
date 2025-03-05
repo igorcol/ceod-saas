f6d96a94573fb770c3aa1d10554a881215ad31a8
igorcol <igor.colombini@gmail.com>
2025-03-05 15:54:42 -0300

    feat(emailsPage): Sent Emails Table

--- src/app/emails/page.tsx
+++ src/app/emails/page.tsx
@@ -2,35 +2,61 @@
 import { Button } from "@/components/ui/button";
 import { GetEmailsFromDb } from "@/lib/api";
 import { ApiSendEmails } from "@/lib/Email/_requests/SendEmails";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  interface EmailResult {
+    email: string;
+    success: boolean;
+    error?: { response: string };
+  }
+
+  const [sentEmails, SetSentEmails] = useState<EmailResult[]>([]);
 
   async function handleSendEmails() {
     const emails = await GetEmailsFromDb();
     const results = await ApiSendEmails(emails);
 
-    interface EmailResult {
-      email: string;
-      success: boolean;
-      error?: { response: string };
-    }
-
     if (results && results.results) {
-      results.results.forEach((result: EmailResult) => {
-        if (result.success) {
-          console.log(`✔️\t${result.email}`);
-        } else {
-          const error = result.error as { response: string };
-          console.log(`❌\t${result.email}\t->\t${error.response}`);
-        }
-      });
+      SetSentEmails(results.results);
+
+      // results.results.forEach((result: EmailResult) => {
+      //   if (result.success) {
+      //     console.log(`✔️\t${result.email}`);
+      //   } else {
+      //     const error = result.error as { response: string };
+      //     console.log(`❌\t${result.email}\t->\t${error.response}`);
+      //   }
+      // });
     }
   }
+
+  useEffect(() => {
+    console.log(sentEmails);
+  }, [sentEmails]);
 
   return (
     <div className="space-y-5">
       <Button onClick={() => handleSendEmails()}>Enviar Emails</Button>
-      <p>Não implementado</p>
+      <div>
+        <h1 className="text-xl font-bold">Emails Enviados:</h1>
+
+        {sentEmails.map((email) => {
+          return (
+            <div key={email.email} className="mt-3">
+              <div
+                className=" w-[screen] flex flex-row border border-border p-2 gap-x-3"
+              >
+                <p className="border-r border-border pr-3">
+                  {email.success ? "✅" : "❌"}
+                </p>
+                <p>{email.email ? email.email : "- Usuário sem Email -"}</p>
+              </div>
+              <div>{email.error && <p className="text-xs text-muted-foreground">{email.error.response}</p>}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
