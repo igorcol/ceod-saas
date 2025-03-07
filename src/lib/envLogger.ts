2ea674ad52283ac632f0158fb9d9205db409cf31
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-07 03:32:35 -0300

    Enviroment handler with start script

---
+++ src/lib/envLogger.ts
@@ -0,0 +1,10 @@
+const environment = process.env.NODE_ENV;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+console.log(`
+===============================
+🚀 Iniciando aplicação Next.js
+🌍 Ambiente: ${environment}
+🔗 API_URL: ${apiUrl}
+===============================
+`);
