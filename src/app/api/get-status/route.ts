ac431024f29553e0f63ed02460469f97cda7a5cd
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-07 00:11:25 -0300

    feat: Check Api Status

---
+++ src/app/api/get-status/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { StatusCodes } from "http-status-codes";
+
+export async function GET() {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/health`);
+        //console.log(`${process.env.NEXT_PUBLIC_API_URL}/health -> ${response.status}`);
+
+        if (response.status === 200) {
+            return NextResponse.json(
+                { status: "API ONLINE" }, 
+                { status: StatusCodes.OK } 
+            );
+        } 
+        else {
+            return NextResponse.json(
+                { status: "API OFFLINE" },
+                { status: StatusCodes.SERVICE_UNAVAILABLE } 
+            );
+        }
+    } 
+    catch (error) {
+        // if (error instanceof Error) {
+        //     console.log(`❌ Erro ao checkar status da api ->`, error.message);
+        // }
+        return NextResponse.json(
+            { status: "API OFFLINE", message: error },
+            { status: StatusCodes.SERVICE_UNAVAILABLE } 
+        );
+    }
+}
+
--- src/components/sidebar/page.tsx
+++ src/components/sidebar/page.tsx
@@ -1,56 +1,98 @@
-"use client"
+"use client";
 import {
-    Sidebar as SidebarComponent,
-    SidebarContent,
-    SidebarFooter,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem
-} from "@/components/ui/sidebar"
-import { FileSpreadsheet, Home, Mail, QrCode } from "lucide-react"
-import { usePathname } from "next/navigation"
-import Link from "next/link"
+  Sidebar as SidebarComponent,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { FileSpreadsheet, Home, Mail, QrCode } from "lucide-react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
-import ThemeToggle from "@/components/theme-provider/_components/theme-toggle"
+import ThemeToggle from "@/components/theme-provider/_components/theme-toggle";
+import { useEffect, useRef, useState } from "react";
 
 const menuItems = [
-    { icon: Home, label: "Inicio", href: "/" },
-    { icon: FileSpreadsheet, label: "Converter Planilha", href: "/spreadsheet" },
-    { icon: QrCode, label: "Gerar Ingressos", href: "/ingressos" },
-    { icon: Mail, label: "Enviar Emails", href: "/emails" },
-  ]
+  { icon: Home, label: "Inicio", href: "/" },
+  { icon: FileSpreadsheet, label: "Converter Planilha", href: "/spreadsheet" },
+  { icon: QrCode, label: "Gerar Ingressos", href: "/ingressos" },
+  { icon: Mail, label: "Enviar Emails", href: "/emails" },
+];
 
 export default function Page() {
-    const pathname = usePathname()
+  const pathname = usePathname();
 
-    return (
-        <SidebarComponent>
+  const [isApiOnline, setApiOnline] = useState<boolean | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-            <Link href="/" className="block">
-                <SidebarHeader className="p-4">
-                    <h2 className="text-xl font-bold">Gerador de Ingressos</h2>
-                </SidebarHeader>
-            </Link>
+  useEffect(() => {
+    const checkApiStatus = async () => {
+      try {
+        const response = await fetch("/api/get-status");
+        //console.log('RESPONSE STATUS:', response.status)
+        
+        const newStatus = response.ok;
+        setApiOnline((prevStatus) =>
+          prevStatus !== newStatus ? newStatus : prevStatus  // Só altera o estado se for diferente
+        );
 
-            <SidebarContent>
-                <SidebarMenu>
-                    {menuItems.map((item) => (
-                        <SidebarMenuItem key={item.href}>
-                            <SidebarMenuButton asChild isActive={pathname === item.href}>
-                                <Link href={item.href} className="flex items-center
-                                 gap-2">
-                                    <item.icon className="h-5 w-5"/>
-                                    {item.label}
-                                </Link>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    ))}
-                </SidebarMenu>
-            </SidebarContent >
-            <SidebarFooter className="p-4">
-                <ThemeToggle/>
-            </SidebarFooter>
-        </SidebarComponent>
-    )
+        //console.log('-> Is Api Online? >', newStatus)
+      } 
+      catch {
+        setApiOnline(false);
+      }
+    };
+
+    // Chamada inicial
+    checkApiStatus();
+
+    // Configura o intervalo apenas uma vez
+    intervalRef.current = setInterval(checkApiStatus, 5000);
+
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  return (
+    <SidebarComponent>
+      <Link href="/" className="block">
+        <SidebarHeader className="p-4">
+          <h2 className="text-xl font-bold">Gerador de Ingressos</h2>
+        </SidebarHeader>
+      </Link>
+
+      <SidebarContent>
+        <SidebarMenu>
+          {menuItems.map((item) => (
+            <SidebarMenuItem key={item.href}>
+              <SidebarMenuButton asChild isActive={pathname === item.href}>
+                <Link
+                  href={item.href}
+                  className="flex items-center
+                                 gap-2"
+                >
+                  <item.icon className="h-5 w-5" />
+                  {item.label}
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarContent>
+      <SidebarFooter className="p-4">
+        <div className={isApiOnline ? "text-green-600" : "text-red-700"}>
+          {isApiOnline === null
+            ? "Verificando..."
+            : isApiOnline
+            ? "API ONLINE"
+            : "API OFFLINE"}
+        </div>
+        <ThemeToggle />
+      </SidebarFooter>
+    </SidebarComponent>
+  );
 }
