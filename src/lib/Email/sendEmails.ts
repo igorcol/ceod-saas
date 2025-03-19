765b1b58dca08845021a8b157b1b5b1f092129a4
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-03-19 00:00:22 -0300

    feat: DELAY before SendEmail()

--- src/lib/Email/sendEmails.ts
+++ src/lib/Email/sendEmails.ts
@@ -22,6 +22,8 @@ import { imagePath, transporter } from './config';
 ! =================================================================== 
 */
 
+const DELAY = (ms: number) => new Promise(resolve => setTimeout(resolve, ms)) // DEFINE O DELAY
+
 function pathExists(path: string) {
     return fs.existsSync(path)
 }
@@ -52,6 +54,7 @@ export async function SendEmail(to: string, ticketName: string) {
     };
 
     try {
+        await DELAY(2000) // ESPERA 2 SEGUNDOS ANTES DE ENVIAR O PRÓXIMO EMAIL
         await transporter.sendMail(mailOptions)
         //console.log(`✔️\t ${mailOptions.to} \t |\t Enviado`)
         return { 
